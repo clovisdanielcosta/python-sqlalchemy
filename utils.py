1bf2b40d153574db933726cb4c16a77c66054164
clovisdanielcosta <58163677+clovisdanielcosta@users.noreply.github.com>
2020-07-06 00:37:00 -0300

    Alterado REST API para fazer autenticação no banco de dados. Em models foi criado tabela de usuário com login e senha para autenticação na API. Incluído em requirements a extensão flask-httpauth.

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-from models import Pessoas, db_session
+from models import Pessoas, Usuarios
+
+### CRUD nas tabelas
 
 def insere_pessoa():
     pessoa = Pessoas(nome='Costa', idade=47)
     print(pessoa)
     pessoa.save()
-
-### CRUD na tabelas
 
 def consulta_pessoa():
     pessoa = Pessoas.query.all()
@@ -23,10 +23,22 @@ def exclui_pessoa():
     pessoa = Pessoas. query.filter_by(nome='Clovis').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 ###
 
+
 if __name__ == '__main__':
+    insere_usuario('clovis', '123')
+    insere_usuario('daniel', '321')
+    consulta_todos_usuarios()
     #insere_pessoa()
     #altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoa()
+    #exclui_pessoa()
+    #consulta_pessoa()
